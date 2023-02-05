a0e7a6693e6ad9c31ce9bf446c44fddd1f0749b3
pixelHat <alisson42@protonmail.com>
2023-02-05 16:04:59 -0300

    Add Symbol Table

---
+++ translator/symbol-table.lua
@@ -0,0 +1,37 @@
+local SymbolTable = {
+	table = {
+		["SCREEN"] = 163384,
+		["KBD"] = 24576,
+		["SP"] = 0,
+		["LCL"] = 1,
+		["ARG"] = 2,
+		["THIS"] = 3,
+		["THAT"] = 4,
+	},
+	next_empty_variable_spot = 16,
+}
+SymbolTable.__index = SymbolTable
+
+for i = 1, 16 do
+	SymbolTable.table["R" .. (i - 1)] = i - 1
+end
+
+function SymbolTable.new()
+	local instance = setmetatable({}, SymbolTable)
+	return instance
+end
+
+function SymbolTable.get(self, key)
+	local value = self.table[key]
+	if not value then
+		value = self.next_empty_variable_spot
+		self.next_empty_variable_spot = self.next_empty_variable_spot + 1
+	end
+	return value
+end
+
+function SymbolTable.add_label(self, key, line)
+	self.table[key] = line
+end
+
+return SymbolTable
